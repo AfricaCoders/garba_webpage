f66f50405872a216ad5a1824adc949bc4dfb48c8
WorkStation <workstation@MacBook-Pro-de-EPONON.local>
2022-03-20 18:53:51 +0200

    Add smooth scrolling effect & hamberger mobile view menu

--- js/menu.js
+++ js/menu.js
@@ -1,21 +1,43 @@
 const mobile_menu_open = document.createElement('div');
+const cross = document.createElement('div');
+const nav_container_mobile = document.createElement('div');
+const ul = document.createElement('ul');
+
+cross.className = 'cross-container';
+cross.innerHTML = `<img src="materials/img/cross-sign.png" class="cross-sign-menu" />`;
+
+ul.innerHTML = `
+    <li>
+        <a href="index.html">Home</a>
+    </li>
+    <li>
+        <a href="about.html">About</a>
+    </li>`;
+nav_container_mobile.className="nav-mobile";
 mobile_menu_open.className = 'mobile-menu';
+
+
+mobile_menu_open.appendChild(cross);
+nav_container_mobile.appendChild(ul);
+mobile_menu_open.appendChild(nav_container_mobile);
+
 document.querySelector('body').appendChild(mobile_menu_open);
 
 const menu = document.querySelector('.menu-bar-menu');
 const menu_container = document.querySelector('.menu-bar');
 
 menu.addEventListener('click', showMenu);
+cross.addEventListener('click', hideMenu);
 
 function showMenu(e){
     console.log('clicked');
-    if (mobile_menu_open.style.right == '0'){
-        mobile_menu_open.style.right = '-100%';
-    }
-    else {
-        mobile_menu_open.style.right = '0';
-    }    
-        
-    menu_container.innerHTML = `<img src="materials/img/cross-sign.png" class="menu-bar-menu" alt="close menue"/>`;   
+    mobile_menu_open.style.right = '0';
+         
     e.preventDefault();
 }
+
+function hideMenu(e){
+    mobile_menu_open.style.right = '-100%';
+    e.preventDefault();
+}
+
