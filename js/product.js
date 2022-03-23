0dfa45f2ee1897530d3908ceee17438d4dced424
WorkStation <codewithafrica@gmail.com>
2022-03-23 16:26:15 +0200

    Improove star review ui

--- js/product.js
+++ js/product.js
@@ -3,8 +3,9 @@ let orders = '';
 let name_seller = '';
 
 const sellers_contain = document.querySelector('.sellers');
-const review_contain = document.querySelector('.review-stars');
-const inner_stars = document.createElement('span');
+
+const img_star = '<img src="materials/img/favourite.png" class="star" />';
+
 
 async function getInfo(){
     fetch('data.json')
@@ -14,31 +15,40 @@ async function getInfo(){
     .then(data => {
         console.log(data)
         data.forEach(seller => { 
+            const seller_contain = document.createElement('div');
+            seller_contain.className = "seller";
+            const profile_pic = document.createElement('span');
+            profile_pic.className = 'img-seller-container';
+            profile_pic.innerHTML = "<img src='materials/img/images.jpeg' alt=''>";
+
+            const star_contain = document.createElement('span');
+            star_contain.className="star_container";
+            star_contain.innerHTML ='';
+
+            const info_box = document.createElement('div');
+            info_box.className = 'info';
+
+            seller_contain.appendChild(profile_pic);
+            seller_contain.appendChild(info_box);
+            const review = document.createElement('div');
+            review.className = 'review-stars';
+            review.appendChild(star_contain);
+            info_box.appendChild(review);
+
             console.log(seller.stars);
             for(let i = 0; i < seller.stars; i ++){
-                inner_stars.innerText += `<span><img src="materials/img/favourite.png"  class="star" alt="" srcset=""></span>`;
-                console.log('yes');     
+                star_contain.innerHTML += img_star;
+                
             }
-            console.log(inner_stars);
-
-            sellers_contain.innerHTML += `
-            <div class="seller">
-                <span class="img-seller-container">
-                    <img  src="materials/img/images.jpeg" alt="">
-                </span>
-                <div class="info">
-                    <div class="review-stars">
-                        <span><p class="number-star">${seller.stars}</p> <img src="materials/img/favourite.png"  class="star" alt="" srcset=""></span>
-                    </div>
-                    <!-- fetch data from data.json -->
-                    <p>price: ${seller.price}</p>
-                    <p>orders: <span class="markey">${seller.orders}</span></p>
-                    <p>seller: ${seller.name}</p>
-                </div>
-                <div class="action"><button class="order"><a href="product.html">Commander</a></button></div>
+            info_box.innerHTML += `
+            <p>price: ${seller.price}</p>
+            <p>orders: <span class="markey">${seller.orders}</span></p>
+            <p>seller: ${seller.name}</p>
             </div>
-            `;
+            <div class="action"><button class="order"><a href="product.html">Commander</a></button></div>
 
+            `;
+            sellers_contain.appendChild(seller_contain);
         })
     });
 }
